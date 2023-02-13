846a2ec6859dc4621931f5450567519ceb9eced8
Joshua Botha <JBotha1951@gmail.com>
2023-02-13 10:33:37 +0200

    Restructured files and folder and names

--- Setup Step 2 - Create Python Venv.bat
+++
@@ -1,8 +0,0 @@
-ECHO OFF
-ECHO Creating Virtual Environment
-"python" "-m" "venv" "./venv"
-ECHO Update pip
-"./venv/Scripts/python.exe" "-m" "pip" "install" "--upgrade" "pip"
-ECHO Installing Python modules
-"./venv/Scripts/pip3.exe" "install" "numpy" "jupyterlab" "pyspark"
-PAUSE
---
+++ Setup/# Step 2 - Create Python Venv.bat
@@ -0,0 +1,17 @@
+ECHO OFF
+ECHO ***********************************************************************
+ECHO Installing Virtual environment for JupyterLab based Pyspark Development
+ECHO ***********************************************************************
+CD ..
+IF EXIST "venv\" (
+    ECHO Virtual environment directory exists.
+) ELSE (
+    ECHO Creating Virtual Environment
+    "python" "-m" "venv" "venv"
+    ECHO Update pip
+    "venv\Scripts\python.exe" "-m" "pip" "install" "--upgrade" "pip"
+    ECHO Installing Python modules
+    "venv\Scripts\pip3.exe" "install" "-r" "Setup\requirements.txt"
+    ECHO Virtual environment created!!! (If no errors occured =)
+)
+PAUSE
